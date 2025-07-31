dbe4efdf6acaa933588762d1a2cc467f6daaf687
mtr-prabhat-sharma <142592099+mtr-prabhat-sharma@users.noreply.github.com>
2025-07-31 23:00:04 +0530

    Material table with pagination

--- dynamic-header-table-with-pagination/src/app/materialtable/materialtable.component.ts
+++ dynamic-header-table-with-pagination/src/app/materialtable/materialtable.component.ts
@@ -1,11 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, viewChild } from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
+import { TodosService } from '../todos.service';
+import { Todos } from '../todos';
+import { NgFor, UpperCasePipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-materialtable',
-  imports: [],
+  standalone: true,
+  imports: [MatTableModule, MatPaginatorModule, UpperCasePipe, NgFor],
   templateUrl: './materialtable.component.html',
   styleUrl: './materialtable.component.css'
 })
 export class MaterialtableComponent {
+constructor(private todos: TodosService) {}
 
+  todosList =  new MatTableDataSource<Todos>();
+ tableHeaders: any[] = [];
+ @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngOnInit(): void {
+    this.todos.getTodos().subscribe((res) => {
+      this.todosList.data = res;
+       this.tableHeaders = this.getTableHeaders(this.todosList.data).filter(header => header !== 'userId');
+    });
+  }
+
+   ngAfterViewInit(): void {
+    this.todosList.paginator = this.paginator;
+  }
+    getTableHeaders(data: Todos[]): string[] {
+    const keySet = new Set<string>();
+    data.forEach((item) => {
+      Object.keys(item).forEach((key) => {
+        keySet.add(key);
+      });
+    });
+    return [...keySet];
+  }
 }
--- dynamic-header-table-with-pagination/src/app/plainhtml/plainhtml.component.ts
+++ dynamic-header-table-with-pagination/src/app/plainhtml/plainhtml.component.ts
@@ -1,4 +1,4 @@
-import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
+import { NgClass, NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Todos } from '../todos';
 import { TodosService } from '../todos.service';
@@ -6,7 +6,7 @@ import { TodosService } from '../todos.service';
 @Component({
   selector: 'app-plainhtml',
   standalone: true,
-  imports: [NgFor, NgIf, UpperCasePipe],
+  imports: [NgFor, NgIf, UpperCasePipe, NgClass],
   providers: [TodosService],
   templateUrl: './plainhtml.component.html',
   styleUrl: './plainhtml.component.css',
@@ -15,11 +15,14 @@ export class PlainhtmlComponent implements OnInit {
   constructor(private todos: TodosService) {}
   todosList: Todos[] = [];
   tableHeaders: any[] = [];
+  paginatedData: Todos[] = [];
+  currentPage: number = 1;
   ngOnInit(): void {
     this.todos.getTodos().subscribe((res) => {
       console.log('todos', res);
       this.todosList = res;
       this.tableHeaders = this.getTableHeaders(this.todosList);
+      this.paginatedData = this.todosList.slice(0,25);
       console.log(this.tableHeaders);
     });
   }
@@ -33,4 +36,21 @@ export class PlainhtmlComponent implements OnInit {
     });
     return [...keySet];
   }
+
+  prevPage() {
+    if(this.currentPage > 1) {
+      this.currentPage--;
+      this.paginatedData = this.todosList.slice((this.currentPage - 1)*25, this.currentPage*25);
+      console.log("prev", this.paginatedData);
+    }
+  }
+
+  nextPage() {
+    console.log("length", this.todosList.length /25)
+    if(this.currentPage < this.todosList.length / 25) {
+      this.currentPage++;
+      this.paginatedData = this.todosList.slice((this.currentPage-1) * 25, this.currentPage * 25)
+      console.log("next", this.paginatedData);
+    }
+  } 
 }
